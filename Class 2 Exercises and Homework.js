bdd9c37780abe2b17908b34b94777d0b841c0e54
danh truong <danhtruongwa@yahoo.com>
2023-10-20 11:56:08 -0700

    continue assigment

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -18,20 +18,23 @@ const myself = {
 
 
 // 2. console.log best friend's firstName and your favorite food
-
+console.log(myself.bestFriend.firstName);
+console.log(myself['favorite food']);
 
 // 3. Create an array to represent this tic-tac-toe board
 // -O-
 // -XO
 // X-X
 
+const gameBoard = [['-','O','-'],['-','X','O'],['X','-','X']];
+
 
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
-
+gameBoard[0][2] = 'O';
 
 // 5. Log the grid to the console.
-
+console.log(gameBoard);
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
 // Should be 1 or more characters, then @ sign, then 1 or more characters, then dot, then one or more characters - no whitespace
@@ -44,11 +47,27 @@ const myself = {
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
-const assignmentDate = '1/21/2019';
+
+const assignmentDate = Date.parse('1/21/2019');
+// parses the date string and returns the time since January 1st 1970 in millisecond //
+
+
+const date = new Date(assignmentDate);
+//convert parsed date in milliseconds to Date using new Date()
+console.log(date);
+
 
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
+
+//set dueDate variable base on assignment date//
+const dueDate= new Date('10/17/2023');
+
+// dueDate = assignment date + 7 day 
+dueDate.setDate(dueDate.getDate()+ 7);
+
+
 
 
 // 9. Use dueDate values to create an HTML time tag in format
@@ -71,3 +90,4 @@ const months = [
 
 
 // 10. log this value using console.log
+console.log(dueDate);
