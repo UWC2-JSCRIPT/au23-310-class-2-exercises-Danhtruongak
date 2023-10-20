f07d7be465bfac471995586338b3bc4dbe0c7980
danh truong <danhtruongwa@yahoo.com>
2023-10-20 15:13:33 -0700

    finished assignment 2

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -13,13 +13,14 @@ const myself = {
     lastName: 'Truong',
     'favorite food': 'pizza'
   }
-}
+};
 
 
 
 // 2. console.log best friend's firstName and your favorite food
-console.log(myself.bestFriend.firstName);
-console.log(myself['favorite food']);
+console.log(`answer #2: ${myself.bestFriend.firstName}`);
+console.log(`answer #2: my favorite food is ${myself['favorite food']}`);
+
 
 // 3. Create an array to represent this tic-tac-toe board
 // -O-
@@ -31,9 +32,11 @@ const gameBoard = [['-','O','-'],['-','X','O'],['X','-','X']];
 
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
+
 gameBoard[0][2] = 'O';
 
 // 5. Log the grid to the console.
+
 console.log(gameBoard);
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
@@ -42,33 +45,29 @@ console.log(gameBoard);
 // Hints:
 // - Use rubular to check a few emails: https://rubular.com/
 // - Use regexp test method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+
 let regex = /^[a-zA-Z0-9^\S+]+@[a-zA-Z0-9^\S+]+\.[a-zA-Z0-9^\S+]+$/;
 let string = 'myEmail@yahoo.com';
 let result = regex.test(string);
-console.log(result);
+console.log(`answer #6: ${result}`);
+
+
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
 
-const assignmentDate = Date.parse('1/21/2019');
-// parses the date string and returns the time since January 1st 1970 in millisecond //
 
-
-const date = new Date(assignmentDate);
-//convert parsed date in milliseconds to Date using new Date()
-console.log(date);
-
+const assignmentDate= new Date('1/21/2019');
+console.log(`answer #7: assignment Date is ${assignmentDate}`);
 
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
+ 
+  const dueDate = assignmentDate.setDate(assignmentDate.getDate()+ 7);
 
-//set dueDate variable base on assignment date//
-const dueDate= new Date('10/17/2023');
-
-// dueDate = assignment date + 7 day 
-dueDate.setDate(dueDate.getDate()+ 7);
-
+  const resultDueDate = new Date(dueDate);
+  
 
 
 
@@ -92,4 +91,4 @@ const months = [
 
 
 // 10. log this value using console.log
-console.log(dueDate);
+  console.log(`answer #10: ${resultDueDate}`);
